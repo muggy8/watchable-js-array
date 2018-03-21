0c7bf49652364c4cc83ea8f2e73dd1cc766bace8
muggy8 <muggy8@gmail.com>
2018-03-20 17:02:46 -0700

    filled in the rest of it (mostly copied from the array version)

--- watch-proto-fn.js
+++ watch-proto-fn.js
@@ -1,3 +1,51 @@
-var watchProtoFn = (function(){
-    
+var watchableProtoFn = (function(){
+    function createOverridingProto(obj){
+        var objProto = Object.getPrototypeOf(obj)
+        var overlayProto = Object.create(objProto)
+
+        while(objProto){
+            Object.getOwnPropertyNames(objProto).forEach(function(calledProto, prop){
+                (typeof calledProto[prop] === "function") && !overlayProto.hasOwnProperty(prop) && Object.defineProperty(overlayProto, prop, {
+                    configurable: false,
+                    enumerable: false,
+                    value: function(){
+                        var args = Array.prototype.slice.call(arguments)
+                        this.watchers && this.watchers[prop] && this.watchers[prop].forEach(function(cb){
+                            cb(args)
+                        })
+                        return calledProto[prop].apply(this, args)
+                    }
+                })
+            }.bind(null, objProto))
+            objProto = Object.getPrototypeOf(objProto)
+        }
+        
+        !overlayProto.watchers && Object.defineProperty(overlayProto, "watchers", {
+            configurable: false,
+            enumerable: false,
+            value: {}
+        })
+        
+        !overlayProto.watch && Object.defineProperty(overlayProto, "watch", {
+            configurable: false,
+            enumerable: false,
+            value: function(target, callback){
+                if (typeof overlayProto.watchers !== "object"){
+                    return false;
+                }
+
+                overlayProto.watchers[target] = overlayProto.watchers[target] || []
+                overlayProto.watchers[target].push(callback)
+            }
+        })
+
+        return overlayProto
+    }
+
+    return function(obj){
+        var overrideProto = createOverridingProto(obj)
+        Object.setPrototypeOf(obj, overrideProto)
+        return obj
+    }
 })()
+module !== undefined && module.exports = watchableProtoFn
